d65a29e81e1ae51055c62dfb1658cc9c3cf0267e
ryantheamigo <rz.eppy97@gmail.com>
2017-04-04 09:15:04 -0700

    Add files via upload

---
+++ MainSensors.s
@@ -0,0 +1,100 @@
+.global main
+    
+.include "motor_lib.s"
+.include "SENSORS.s"
+.include "timer_and_interrupt_lib.s"
+.include "BUTTON.s"
+.data
+   drive_case: .word forward, forward, forward, forward, forward, forward, forward, pivot_right, forward, forward, forward, pivot_right, forward, pivot_left, pivot_left, forward
+.text
+    
+    
+.ent main
+main:
+    jal setup_sensor
+    jal setup_multi_vector_mode
+    jal setup_timer2_16bit
+    jal setup_output_compare2
+    jal setup_output_compare3
+    jal setup_button
+    jal setup_motor_pins
+    while:
+    JAL get_button
+    LI $t0, 1
+    BEQ $t0, $v1, TURNON
+    J while
+    
+    move $s1, $zero	# initializes prev sensor value
+    
+    TURNON:
+    jal read_sensor
+    move $s0, $v0	# saves sensor value to s0 reg
+    beq $s0, $s1, TURNON    # jumps to turn on and reads sensors if sensor value hasn't changed
+    
+    move $a0, $v0
+    jal movement
+    move $s1, $s0	# moves prev sensor val to s1 reg
+    j TURNON
+.end main
+
+.ent movement
+movement:
+    ADDI $sp, $sp, -4
+    SW $ra, 0($sp)
+    
+    # move $t0, $a0
+    
+    # move $a0, $zero
+    # move $a1, $zero
+    # jal write_to_motors
+    
+    # loads drive_case address
+    la $t1, drive_case
+    sll $t0, $t0, 2	# multiplies index by 4
+    add $t1, $t0, $t1	# offsets index
+    lw $t0, 0($t1)		# loads jump value
+    jr $t0
+    
+    forward:
+    # sets wheels to forward
+    li $t0, 0b10000000
+    sw $t0, LATDSET
+    li $t0, 0b1000000
+    sw $t0, LATDCLR
+
+    # sets to 68% and 69% duty cycle
+    li $a0, 70
+    li $a1, 71
+    jal write_to_motors
+    j end_movement
+    
+    pivot_left:
+    li $t0, 0b11000000
+    sw $t0, LATDCLR
+    
+    # 60%
+    li $a0, 60 # left
+    li $a1, 60 # right
+    jal write_to_motors
+    j end_movement
+    
+    pivot_right:
+    li $t0, 0b11000000
+    sw $t0, LATDSET
+    
+    # 60%
+    li $a0, 60 # left
+    li $a1, 60 # right
+    jal write_to_motors
+    j end_movement
+    
+    end_movement:
+    
+    LW $ra, 0($sp)
+    ADDI $sp, $sp, 4
+    jr $ra
+.end movement
+    
+    
+
+
